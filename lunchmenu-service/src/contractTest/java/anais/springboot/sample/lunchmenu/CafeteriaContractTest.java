1ba957ecc11e56e41e6f5f833e7b4198e4476519
anais.kim <itschstyle@gmail.com>
2017-09-20 01:20:35 +0900

    remove 'AbstractContractTest'

--- lunchmenu-service/src/contractTest/java/anais/springboot/sample/lunchmenu/CafeteriaContractTest.java
+++ lunchmenu-service/src/contractTest/java/anais/springboot/sample/lunchmenu/CafeteriaContractTest.java
@@ -1,20 +1,36 @@
 package anais.springboot.sample.lunchmenu;
 
+import io.restassured.RestAssured;
 import org.apache.http.HttpStatus;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 
-public class CafeteriaContractTest extends AbstractContractTest {
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureStubRunner(ids = {"anais.springboot.sample:cafeteria-service:+:stubs:6060"}, workOffline = true)
+public class CafeteriaContractTest {
+
+    @Before
+    public void setUp() throws Exception {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = 6060;
+        RestAssured.basePath = "/api";
+    }
 
     @Test
     public void whenCallGetCafeteriaExist_thenReturnCafeteriaInformation() throws Exception {
         given()
             .pathParam("cafeteriaId", 4)
         .when()
-            .get("/api/cafeterias/{cafeteriaId}")
+            .get("/cafeterias/{cafeteriaId}")
         .then()
             .statusCode(HttpStatus.SC_OK)
             .body("id", is(4));
@@ -25,7 +41,7 @@ public class CafeteriaContractTest extends AbstractContractTest {
         given()
             .pathParam("cafeteriaId", 999)
         .when()
-            .get("/api/cafeterias/{cafeteriaId}")
+            .get("/cafeterias/{cafeteriaId}")
         .then()
             .statusCode(HttpStatus.SC_NOT_FOUND)
             .body("errorCode", notNullValue())
